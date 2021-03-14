3ee7bcdfb6922cf242d9b04f222f5231cdf42549
harshita501 <khushi.b13@gmail.com>
2021-03-14 08:37:12 -0600

    updated code

--- src/main/java/SeleniumSession1/LaunchAppTest.java
+++ src/main/java/SeleniumSession1/LaunchAppTest.java
@@ -3,15 +3,20 @@ package SeleniumSession1;
 public class LaunchAppTest {
 
 	public static void main(String[] args) {
+
 		System.out.println("remote--launch app");
 
+
 	}
+	
 	public void launch() {
 		System.out.println("remote launch");
 	}
+
   
   public void closeApp(){
     System.out.println("remote close app");
+
 
 }
 }
