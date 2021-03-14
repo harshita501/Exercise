0d1784117e984024a03e73965a4d184431d46970
harshita501 <khushi.b13@gmail.com>
2021-03-14 07:36:18 -0600

    updated code

--- src/main/java/SeleniumSession1/NewMerge.java
+++ src/main/java/SeleniumSession1/NewMerge.java
@@ -3,11 +3,11 @@ package SeleniumSession1;
 public class NewMerge {
 
 	public static void main(String[] args) {
-		System.out.println("this is my remote---merge");
+		System.out.println("this is my local---merge");
 
 	}
 	public void login() {
-		System.out.println("remote--login");
+		System.out.println("local--login");
 	}
 
 }
