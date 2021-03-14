4c07e3ef71e1958e8562df7f067d654bee1ad0e1
harshita501 <59357173+harshita501@users.noreply.github.com>
2021-03-14 08:22:42 -0600

    Update LaunchAppTest.java

--- src/main/java/SeleniumSession1/LaunchAppTest.java
+++ src/main/java/SeleniumSession1/LaunchAppTest.java
@@ -3,14 +3,14 @@ package SeleniumSession1;
 public class LaunchAppTest {
 
 	public static void main(String[] args) {
-		System.out.println("local--launch app");
+		System.out.println("remote--launch app");
 
 	}
 	public void launch() {
-		System.out.println("local launch");
+		System.out.println("remote launch");
 	}
   
   public void closeApp(){
-    System.out.println("close app");
+    System.out.println("remote close app");
 
 }
