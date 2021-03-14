a2c008663209a8637a4277de67b07cc22e839d94
harshita501 <59357173+harshita501@users.noreply.github.com>
2021-03-14 08:12:53 -0600

    Create LaunchAppTest.java

---
+++ src/main/java/SeleniumSession1/LaunchAppTest.java
@@ -0,0 +1,16 @@
+package SeleniumSession1;
+
+public class LaunchAppTest {
+
+	public static void main(String[] args) {
+		System.out.println("local--launch app");
+
+	}
+	public void launch() {
+		System.out.println("local launch");
+	}
+  
+  public void close App(){
+    System.out.println("close app");
+
+}
