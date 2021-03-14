558d3fc08b55d63b45ee4d2962727317ceade322
harshita501 <59357173+harshita501@users.noreply.github.com>
2021-03-14 07:14:59 -0600

    Create NewMerge.java

---
+++ NewMerge.java
@@ -0,0 +1,13 @@
+package SeleniumSession1;
+
+public class NewMerge {
+
+	public static void main(String[] args) {
+		System.out.println("this is my remote---merge");
+
+	}
+	public void login() {
+		System.out.println("remote--login");
+	}
+
+}
